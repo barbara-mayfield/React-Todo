94d61ccb434dade8a0ff30796baa8f11184d508b
Barbara Mayfield <bmayfield1991@gmail.com>
2019-10-01 20:46:38 -0500

    Initial commit, imported react to all files, beginning work on App class component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      task: '',
+      id: Date.now(),
+      completed: false
+    }
+  }
+
+  addTask = (event, taskName) => {
+    event.preventDefault();
+    const newTask = {
+      task: '',
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+
+    })
+  }
+  
   render() {
     return (
       <div>
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
